cb2cb7a933f6da9ad6744ff25c0662e3f8583346
Tamal <onlyjohn98@gmail.com>
2019-06-28 18:50:47 +0530

    new commits for string interpolations and other features

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -2,9 +2,45 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-user',
-    template: '<h2> Hi!! I am a user </h2>'
+    templateUrl: './user.component.html',
+    styleUrls: ['./user.component.css']
 })
 
 export class UserComponent{
-     
+    /* declaring the properties & assigning datatypes to those properties */
+    firstName : String;
+    lastName : String;
+    age : Number;
+    address;
+    intArray : Number[];
+    hasGf : Boolean;
+
+    /* decaring constuctor for the class and assigning values to the properties 
+    while the class is getting instantiated */
+    constructor(){
+        console.log('component has been initialized');
+        this.firstName = 'Tamal';
+        this.lastName = 'Das';
+        this.age = 25;
+        this.address = {
+            streetNo : '5',
+            street :'Selimpur Lane',
+            area : 'Dhakuria',
+            pin : '700031'
+        }
+        this.intArray = [1,2,3,4,5];
+        this.hasGf = true
+    }
+    //declaring methods of that class
+    method1(){
+        console.log(`First name is ${this.firstName}`);
+    }
+
+    method2(){
+        console.log(`Second name is ${this.lastName}`);
+    }
+
+    showPincode(){
+        return this.address.pin;
+    }
 }
